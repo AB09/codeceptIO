0415b116ea01430637e1085016f42eb0b40a71bf
AB09 <abhishekqa09@gmail.com>
2019-04-06 17:09:28 -0400

    added new elements and updated open method

--- pages/PIP.js
+++ pages/PIP.js
@@ -7,8 +7,23 @@ module.exports = {
   productTitle:"[data-automation='product-title']",
   productDesc: "[data-automation='short-description']",
   productPrice:"[data-automation='buybox-price']",
+  prodcutRatings: "[class*='css-0'] [data-automation='rating-stars']",
+  qtyDecreaseBtn:"[data-automation='decrease-qty']",
+  qtyIncreaseBtn:"[data-automation='increase-qty']",
+  qtyInputBox:"[data-automation='quantity'] input",
 
-  open(skuId='6000198160418') {
+
+
+  //cta buttons
+  cta:{
+  	addToRegs:"[data-automation='heart-icon']",
+  	addToCart:"[data-automation='cta-button']",
+  	findInStore:"[data-automation='find-in-store']"	
+  }
+  
+
+
+  open(skuId='conair-1875w-mid-size-hair-dryer/6000198160418') {
   	I.amOnPage(skuId);
   },
 
