66a5db415ed3297451302ff861b324428b7aee5f
AB09 <abhishekqa09@gmail.com>
2019-04-06 15:45:41 -0400

    default conf added to file

---
+++ codecept.conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://www.walmart.ca/en/ip/',
+      show: true,
+      windowSize:'1680x1050',
+      // resizeWindow:'maximize'
+    }
+  },
+  include: {
+    I: './steps_file.js',
+    pipPage: './pages/PIP.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: '.bin'
+}
