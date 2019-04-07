8df606564c0df44f6cdc0b01505b5f5079cb6cdc
AB09 <abhishekqa09@gmail.com>
2019-04-07 14:00:57 -0400

    Added test to verify increase quantity and named test and used before hook

--- tests/test1_test.js
+++ tests/test1_test.js
@@ -1,13 +1,24 @@
 
 Feature('Testing item page'); 
-	Scenario('test something', (I, pipPage) => {
-		pipPage.open();
+	
+	Before((pipPage) => {
+  		pipPage.open();
+	});
+
+	Scenario('Verify static elements of item page', (pipPage) => {
 		pipPage.hasLoaded();
 		pipPage.verifyCommonElements();
 	});
 
-	Scenario.only('get product item title', (I, pipPage) => {
-		 pipPage.open();
+	Scenario('Add item to cart and verify quantity in cart', (pipPage) => {
 		 pipPage.addItemToCart();
 		 pipPage.verifyCartQuantity(1);
 	});
+
+	Scenario('Increase quantity on item page and add item to cart', (pipPage) => {
+		 pipPage.increaseQtyBy(2);
+		 pipPage.addItemToCart();
+		 pipPage.verifyCartQuantity(3);
+	});
+
+
