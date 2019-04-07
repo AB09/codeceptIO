a7cfe38564fa92a430260870473aab31012a3d06
AB09 <abhishekqa09@gmail.com>
2019-04-07 12:34:13 -0400

    using chai and assert and function to verfiy quantity

--- pages/gmHeader.js
+++ pages/gmHeader.js
@@ -1,14 +1,20 @@
 const I = actor();
+const chai = require('chai');  
+const assert = chai.assert; 
+const recorder = require('codeceptjs').recorder;
 
 module.exports = {
 
   // insert your locators and methods here
   gmHeader:"[class*='gm-header']",
   navCartCount: "#nav-cart-mobile .nav-cart-count",
-
-  async getCartCount() {
-  		I.waitForElement(this.gmHeader);
-  		await I.waitForElement(this.navCartCount,30);
-  		console.log(`${await I.grabTextFrom(this.navCartCount)}`);
+  
+  async verifyQuantity(expectedQty) {
+  	let qtyCount = await I.grabTextFrom(this.navCartCount);
+  	try {
+  	assert.equal(qtyCount,expectedQty,`FAIL: Quantity Mismatched, cart count is not matching the expected value`);
+  } catch (e) {
+    recorder.throw(e);
   }
+  },
 }
