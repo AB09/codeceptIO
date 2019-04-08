ce7a7fd61798bc2455b683120ef09d876238e4ea
AB09 <abhishekqa09@gmail.com>
2019-04-07 22:34:09 -0400

    added new function and elements

--- pages/fulfillment.js
+++ pages/fulfillment.js
@@ -1,13 +1,43 @@
 const I = actor();
+const chai = require('chai');  
+const assert = chai.assert; 
+const recorder = require('codeceptjs').recorder;
 
 module.exports = {
 
   pickUpTab: "#pickup-section",
   shippingTab: "#shipping-section",
+  locateStore1: "#pickup-section [class*='css-p42mmv']",
+  postalCodeInputPickup: "#pickup-section input",
+  postalCodeUpdateBtnPickup: "#pickup-section button",
+  pickupStoreElements: "#pickup-section div[class*='css-1yp4ln']",
+   
+  	async exandPickupTab() {
+  		await I.click(this.pickUpTab);
+  	},
 
-  async checkFreeBtn() {
-    I.click(this.pickUpTab);
-    console.log(`${await I.grabTextFrom(locate("#pickup-section [class*='css-p42mmv']").first())}`);
+  async enterPostalCode(pc="M9V3G4") {
+  	this.exandPickupTab();
+  	I.waitForVisible(this.postalCodeInputPickup);
+  	I.fillField(this.postalCodeInputPickup, pc);
+  	I.waitForEnabled(this.postalCodeUpdateBtnPickup);
+  	await I.click(this.postalCodeUpdateBtnPickup);
+  },
+
+  async verifyNearestStores() {
+  	I.waitForElement(this.pickupStoreElements,10);
+  	await I.seeNumberOfVisibleElements(this.pickupStoreElements, 3);
+  },
+
+  async getFirstNearestLocation() {
+  	let arr;
+  	 	arr=await I.grabTextFrom({xpath:"(//section[@id='pickup-section']//div[contains(@class,'css-1yp4ln')]//following::b)[1]"});
+  	try {
+  		assert.equal(arr,'Walmart Rexdale Supercentre',"'FAIL: Nearest store is not Walmart Rexdale Supercentre'");	
+  	} catch(e) {
+  		recorder.throw(e);
+  	}
+  	
   }
 
 }
