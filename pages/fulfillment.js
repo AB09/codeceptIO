c2114100354ebb1dc9aa17e57868a30fd503b937
AB09 <abhishekqa09@gmail.com>
2019-04-09 07:13:50 -0400

    added new elements and function for shipping tab

--- pages/fulfillment.js
+++ pages/fulfillment.js
@@ -10,10 +10,14 @@ module.exports = {
   shippingTab: "#shipping-section",
   locateStore1: "#pickup-section [class*='css-p42mmv']",
   postalCodeInputPickup: "#pickup-section input",
-  postalCodeUpdateBtnPickup: "#pickup-section button",
+  postalCodeUpdateBtnPickup: "#pickup-section button[type='submit']",
   pickupStoreElements: "#pickup-section div[class*='css-1yp4ln']",
   pickupStoreInfoList: "#pickup-section div[class*='css-1yp4ln'] b",
-   
+  pcInputShippingTab: "#shipping-section input",
+  pcUpdateBtnShippingTab: "#shipping-section button[type='submit']",
+  shippingTypeText: "#shipping-section [class*='css-1y3tu5o']",
+  shippingFeeText: "#shipping-section [class*='css-1movlma']",
+
   	async exandPickupTab() {
   		await I.click(this.pickUpTab);
   	},
@@ -31,7 +35,7 @@ module.exports = {
   	await I.retry({ retries: 2, minTimeout: 10000 }).seeNumberOfVisibleElements(this.pickupStoreElements, 3);
   },
 
-  async getFirstNearestLocation() {
+  	async getFirstNearestLocation() {
   	let arr;
   	 	arr=await I.grabTextFrom(this.pickupStoreInfoList);
   	try {
@@ -39,7 +43,23 @@ module.exports = {
   	} catch(e) {
   		recorder.throw(e);
   	}
-  	
-  }
+  },
 
+  	exandShippingTab() {
+		I.click(this.shippingTab);
+	},
+
+	async enterShippingPC(pc="M9V3G4") {
+		this.exandShippingTab();
+		I.waitForVisible(this.pcInputShippingTab);
+		I.fillField(this.pcInputShippingTab,pc);
+		I.waitForEnabled(this.pcUpdateBtnShippingTab);
+		await I.click(this.pcUpdateBtnShippingTab);
+	},
+	async verifyShippingFees(cost=data.shippingType.standard.cost) {
+		let shippingType, shippingFee;
+		I.retry({ retries: 2, minTimeout: 10000 }).waitForVisible(this.shippingTypeText);
+		I.see(data.shippingType.standard.label,{css:this.shippingTypeText});
+		I.see(cost,{css:this.shippingFeeText});
+   }
 }
