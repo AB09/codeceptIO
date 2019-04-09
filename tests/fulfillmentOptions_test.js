fcf0a57a007a011e546b279246cf74f08c8ccd39
AB09 <abhishekqa09@gmail.com>
2019-04-08 21:01:03 -0400

    using Data option to traverse trhough different data

--- tests/fulfillmentOptions_test.js
+++ tests/fulfillmentOptions_test.js
@@ -1,12 +1,15 @@
-
+const data = require('../testdata/data');
 Feature('Pickup and Shipping options');
 
-Before((pipPage) => {
-  		pipPage.open();
-	});
+let skuIdArr = new DataTable(['skuID', 'prTitle']);
+skuIdArr.add([data.item1.sku,data.item1.title]);
+skuIdArr.add([data.item2.sku,data.item2.title]);
+skuIdArr.add([data.item3.sku,data.item3.title]);
 
-Scenario.only('Test Pickup tab', (pipPage) => {
+Data(skuIdArr).Scenario('Test Pickup tab', (pipPage,current) => {
+	pipPage.open(current.skuID)
 	pipPage.hasLoaded();
+	pipPage.verifyCommonElements(current.prTitle);
 	pipPage.fulfillmentModal.enterPostalCode();
 	pipPage.fulfillmentModal.verifyNearestStores();
 	pipPage.fulfillmentModal.getFirstNearestLocation();
