c26c1ac7f8338c2748f2eabe4f15f1ff78239ab2
AB09 <abhishekqa09@gmail.com>
2019-04-09 23:25:00 -0400

    added tags and using sku id from data.json file

--- tests/variantAddToCart_test.js
+++ tests/variantAddToCart_test.js
@@ -4,14 +4,14 @@ const data = require('../testdata/data').variantItem;
 Feature('Verification of add to cart for avaiable/unavailable variant');
 
 Before((pipPage) => {
-  		pipPage.open('6000074498690');
+  		pipPage.open(data.sku);
 	});
 
-Scenario('Try to add available variant item', (pipPage) => {
+Scenario.only('Try to add available variant item', (pipPage) => {
 		pipPage.addAvailableVariantItem();
 		pipPage.verifyCartQuantity(1);
-});
+}).tag('variant').tag('pip');
 
 Scenario('Try to add unavailable variant item', (pipPage) => {
 		pipPage.addUnavailableVariantItem();
-});
+}).tag('variant').tag('pip');
